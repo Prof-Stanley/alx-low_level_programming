953b500a189110348a6bec20a7cfee358789c870
Prof-Stanley <st.profisaacjnr@gmail.com>
2022-09-13 05:10:47 -0700

    I am yet to become a software engineer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
- * main - entry point
- * Return: one
+ *main-program entry point.
+ *
+ *Return: 1 on success.
  */
 int main(void)
 {
-        write(STDOUT_FILENO, "and that piece of art is useful"
-                        \"
-                - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
