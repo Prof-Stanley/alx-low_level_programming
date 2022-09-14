66803984f6bc0a8cace40c9d8e33b34db7fcc073
Prof-Stanley <st.profisaacjnr@gmail.com>
2022-09-14 14:26:45 -0700

    I am just keep my head above water level

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet 
-*/
-
+ * print_alphabet - Entry point
+ * Description: a function that prints the alphabet,
+ * in lowecase followed by a new line
+ * Return: void
+ */
 void print_alphabet(void)
 {
-    int ch;
+	char letter = 'a';
 
-    for (ch = 'a'; ch <= 'z'; ++ch)
-        _putchar(ch);
-    _putchar('\n');
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
 }
