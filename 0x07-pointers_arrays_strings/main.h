13f7b94922df98da4e84edcb955090e2bbd3e628
Prof-Stanley <st.profisaacjnr@gmail.com>
2022-09-26 21:30:00 -0700

    I have no choice but to study to grasp this knowledge so I can be competent

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -8,3 +11,5 @@ char *strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
+#endif
