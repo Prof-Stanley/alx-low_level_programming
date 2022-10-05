7238024286b0d829bd4654c6d2d04d3dc2673743
Prof-Stanley <st.profisaacjnr@gmail.com>
2022-10-05 14:11:08 -0700

    Repeat functions with the aim of addressing smaller problems without losing sight of the main problem

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,23 +1,26 @@
 #include "main.h"
-
 /**
- * wildcmp - function that compares two strings
- * @s1: first string
- * @s2: second string that can cointain * which replace any string
- * Return: 1 if matching 0 ortherwise
+ * wildcmp - compares two strings and returns 1
+ * if the strings can be considered identical,
+ * otherwise return 0.
+ * @s1: the normal string
+ * @s2: the special string containing "*"
+ *
+ * Return: 1 if identical, else 0
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s2 == '\0' && *s1 == '\0')
 		return (1);
+
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
+
 	if (*s2 == '*')
-	{
-		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-			return (0);
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
-	}
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
 	return (0);
 }
