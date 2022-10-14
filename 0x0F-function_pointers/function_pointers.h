5c188c6900e96ba606b455c48a5f700c1151a2e4
Prof-Stanley <st.profisaacjnr@gmail.com>
2022-10-14 08:24:47 -0700

    Functions and Pointers need logic

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+int _putchar(char c);
 #include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
