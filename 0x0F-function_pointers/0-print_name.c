894053c5ca14009661676280da7f0ccb9dd1d77d
Prof-Stanley <st.profisaacjnr@gmail.com>
2022-10-14 08:30:13 -0700

    Functions and Pointers need logic

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,16 @@
+#include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * print_name - Print a name from a function pointer
- * @name: char string
- * @f: function pointer that takes a string argument
- */
-
-void print_name(char *name, void (*f)(char *));
+ * print_name - function thats prints a name passed to it
+ * @name: char to display to the stdout
+ * @f: A pointer function
+ * author: Chukwubuikem 
+ * Return: No return cause we've declared VOID as R_TYPE
+*/
+void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f) /* if any is NULL */
+		return;
+
 	f(name);
 }
